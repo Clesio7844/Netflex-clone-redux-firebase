aa427b06308bf08b0a854fd2ee6ddce0b065703e
david <daviddecastro42@live.co.uk>
2021-02-04 17:38:02 +0000

    created nav component and css page, add firebase, Homesecreen to have all the component inside homescreen

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAwHVe7Ps4ylVk2rZx4KbMyvMCtMcGYkJA',
+  authDomain: 'netflix-clone-redux.firebaseapp.com',
+  projectId: 'netflix-clone-redux',
+  storageBucket: 'netflix-clone-redux.appspot.com',
+  messagingSenderId: '448141240664',
+  appId: '1:448141240664:web:30a1486ffca97893d19d2d'
+};
